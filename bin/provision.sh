4462e0b801958d4ee127a095d09ca8695af1b952
John W. Terrell <john@coolpeoplenetworks.com>
2021-09-05 13:54:18 -0700

    Added provision.sh script

---
+++ bin/provision.sh
@@ -0,0 +1,28 @@
+# Debian/Ubuntu Linux provision script
+# Note: this gist is attached to a shortened URL:  http://bit.do/jterrell_config
+# execute with curl -sL http://bit.do/jterrell_config | bash
+sudo apt-get -y install git
+sudo apt-get -y install curl
+
+# Setup local .cfg repository to put parts of $HOME under version control
+# from: https://www.atlassian.com/git/tutorials/dotfiles
+if [ ! -d $HOME/.cfg ]; then
+   git clone --bare https://github.com/john-terrell/home_config.git $HOME/.cfg
+else
+   git pull -C $HOME/.cfg
+fi
+
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
